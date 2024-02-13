dc192889065ba29d3438549d0d1782ea5a3d32a2
Hassan1823 <hassan.zaib223@.com>
2024-02-13 16:05:00 +0500

    pushing

--- app/components/customer-review.jsx
+++ app/components/customer-review.jsx
@@ -1,15 +1,12 @@
-import React from "react";
-import ReviewCard from "./review-card";
 import { ReviewData } from "@/lib/data";
-import { Button } from "@/components/ui/button";
+import ReviewCard from "./review-card";
 
-import Link from "next/link";
 
 const CustomerReview = () => {
   return (
     <div className="flex flex-col items-center justify-center w-full h-auto p-8 text-center">
       <h1 className="text-3xl font-bold text-black lg:text-4xl">
-        Customer's Review
+        {"Customer's Review"}
       </h1>
       <div className="flex flex-wrap items-center justify-center w-full h-auto gap-4 p-4 text-center lg:p-8">
         {ReviewData.slice(0, 6).map((card, index) => (
